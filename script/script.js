bc7895ee37e5349989cf1ce87715d837e61c65a2
Sajmul <sajmul1427@gmail.com>
2024-07-28 11:44:36 +0600

    completed

--- script/script.js
+++ script/script.js
@@ -1,5 +1,18 @@
-const display = document.getElementById('display');
 
-function calculator(v) {
-    display.value += v;
-}
+    function deleteMe() {
+        const display = document.getElementById('display');
+        display.value = '';
+    }
+
+    function calculator(values) {
+        const display = document.getElementById('display');
+        display.value += values;
+    }
+
+    function calculat() {
+        const display = document.getElementById('display');
+        
+        const b = eval(display.value);
+
+        display.value = b;
+    }
