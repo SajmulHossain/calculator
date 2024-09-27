2ddb05a1571684b52326cc80de892ff9a52fafb5
Sajmul Hossain <163473566+SajmulHossain@users.noreply.github.com>
2024-09-28 00:23:45 +0600

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,4 +1,12 @@
-
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(() => {
+      console.log('Service Worker registered successfully.');
+    })
+    .catch(error => {
+      console.log('Service Worker registration failed:', error);
+    });
+}
     function deleteMe() {
         const display = document.getElementById('display');
         display.value = '';
