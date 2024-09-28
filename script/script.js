c0d349e57c31a12bc9ee78f4784c89af73d0d226
Sajmul <sajmul1427@gmail.com>
2024-09-28 14:56:18 +0600

    s

--- script/script.js
+++ script/script.js
@@ -1,12 +1,4 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js')
-    .then(() => {
-      console.log('Service Worker registered successfully.');
-    })
-    .catch(error => {
-      console.log('Service Worker registration failed:', error);
-    });
-}
+
     function deleteMe() {
         const display = document.getElementById('display');
         display.value = '';
